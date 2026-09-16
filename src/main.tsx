4c929dc7c7154ab9fa002f3c1b1798acacb1cc5a
Freelancingonupwork <freelancingonupwork@gmail.com>
2026-09-16 19:00:22 +0530

    update layout

--- src/main.tsx
+++ src/main.tsx
@@ -63,6 +63,7 @@ import "./platform.css";
 import "./theme.css";
 import "./advisor-enhancements.css";
 import "./governance-enhancements.css";
+import "./compact-layouts.css";
 import { FirmAdministration } from "./admin";
 import { AdvisorWorkspace } from "./advisor";
 import { PlatformWorkspace } from "./platform";
